661f0c4b33b2d5e3b5baf50159b78c0f18dd97d9
Duy <10cb4tranbaoduy@gmail.com>
2020-06-25 22:08:04 +0700

    Create creat app todo-list

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return (
+            <div className="doto-item">
+                <p>
+                    Go to shopping
+                </p>        
+            </div>
+        )
+    };
+}
+export default TodoItem;
