71e451b8f375ec40f6b035ce29a825dc257e7616
Duy <10cb4tranbaoduy@gmail.com>
2020-06-26 15:15:24 +0700

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 
 const todoList = [
-  {
-    title: 'Go to Shopping',
-    isComplete: true
-  },
-  {
-    title: 'Go to Market',
-    isComplete: true
-  },
-  {
-    title: 'Go to Bed',
-    isComplete: false
-  }
+  // {
+  //   title: 'Go to Shopping',
+  //   isComplete: true
+  // },
+  // {
+  //   title: 'Go to Market',
+  //   isComplete: true
+  // },
+  // {
+  //   title: 'Go to Bed',
+  //   isComplete: false
+  // }
 ]
 function App(){
   return (
     <div className="App">
       {
-        todoList.map((item, index) => {
-          return <TodoItem key={index} item={item}/>
-        })  
+        todoList.length > 0 && todoList.map((item, index) => {
+          return <TodoItem key={index} item={item} />
+        })
+      }
+      {
+        todoList.length === 0 && <p>Nothing here</p>
       }
     </div>
   );
