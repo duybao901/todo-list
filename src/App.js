4058b47aaaf62e15896e66db9716e2e28da5175d
Duy <10cb4tranbaoduy@gmail.com>
2020-06-26 22:42:58 +0700

    Add event onCLick for div

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import TodoItem from './components/TodoItem';
 
-const todoList = [
-  // {
-  //   title: 'Go to Shopping',
-  //   isComplete: true
-  // },
-  // {
-  //   title: 'Go to Market',
-  //   isComplete: true
-  // },
-  // {
-  //   title: 'Go to Bed',
-  //   isComplete: false
-  // }
-]
-function App(){
-  return (
-    <div className="App">
-      {
-        todoList.length > 0 && todoList.map((item, index) => {
-          return <TodoItem key={index} item={item} />
-        })
-      }
-      {
-        todoList.length === 0 && <p>Nothing here</p>
-      }
-    </div>
-  );
-}
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {  
+      todoList: [
+        {
+        title: 'Go to Shopping',
+        isComplete: false
+        },
+        {
+          title: 'Go to Market',
+          isComplete: false
+        },
+        {
+          title: 'Go to Bed',
+          isComplete: false
+        }
+      ]
+    }
+  }
+  onClickItem(item) {
+    return (event) => {
+      const { todoList } = this.state;
+      const index = todoList.indexOf(item); // Index thang nao khi click
+      const isComplete = item.isComplete; // Lay gia tri hien tai
+      this.setState({ // Tao ra 1 state moi 
+        todoList: [
+          ...todoList.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoList.slice(index+1)
+        ]
+      });
+    }
+  }
+  render() {
+    const { todoList } = this.state;
+      return (     
+        <div className="App">
+          {
+            todoList.length > 0 && todoList.map((item, index) => {
+              return <TodoItem
+                key={index}
+                item={item}
+                onClick={this.onClickItem(item)} />
+            })
+          }
+          {
+            todoList.length === 0 && <p>Nothing here</p>
+          }
+        </div>
+      );
+    }
+  }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import './TodoItem.css';
 class TodoItem extends Component {
-   
     render() {
-        const { item } = this.props;
+        const { item, onClick} = this.props;
+        // const item = this.props.item
         const className = classNames({
             'todo-item': true,
             'todo-item-complete': item.isComplete
         })
-        // const item = this.props.item
         return (
-            <div className = {className}>
+            <div onClick={onClick} className = {className}>
                 <p>
                     {item.title}
                 </p>        
