202670f936425b944a753ced76af7a01c24abdd1
Duy <10cb4tranbaoduy@gmail.com>
2020-06-28 00:17:20 +0700

    Add newItem , onChang+value

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component{
   constructor() {
     super();
     this.state = {  
+      newItem: '',
       todoList: [
         {
         title: 'Go to Shopping',
@@ -23,6 +24,36 @@ class App extends Component{
           isComplete: false
         }
       ]
+    }
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value,
+      //...this.state.todoList
+    })
+  }
+  onKeyUp(event) {
+    const text = event.target.value;
+    if (!text)
+      return;
+    //text = text.trim(); // Xoa ki tu trang thua`
+    // if (!text)
+    //   return;
+    if (event.keyCode === 13) {
+      this.setState({
+        newItem: '',
+        todoList: [        
+          {
+            title: text,
+            isComplete: false
+          },
+          ...this.state.todoList
+        ]
+      })
     }
   }
 
@@ -46,21 +77,28 @@ class App extends Component{
   }
 
   render() {
-    const { todoList } = this.state;
+    const { todoList,newItem } = this.state;
     const checkAll1 = checkAll;
       return (     
         <div className="App">
           <h2 className="app-heading">To Do List</h2>
           <div className="header">
             <img src={checkAll1} />
-            <input type="text" placeholder="Add new item"></input>
+            <input
+              onKeyUp={this.onKeyUp}
+              type="text"
+              value={newItem} // Thang value va thang onchange phai di cung nhau
+              onChange={this.onChange}
+              placeholder="Add new item"></input>
           </div>
           {
             todoList.length > 0 && todoList.map((item, index) => {
               return <TodoItem
                 key={index}
-                item={item}
-                onClick={this.onClickItem(item)} />
+                item={item}                
+                onClick={this.onClickItem(item)}
+                
+              />
             })
           }
           {
